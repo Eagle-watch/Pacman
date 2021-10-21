18e132817e4f2bfe43183da21ff0a192fdc7622e
Francois Bryan <b.francois.gj@gmail.com>
2021-10-21 16:50:46 +0200

    Le plus beau des Pacman

---
+++ JS/pacman.js
@@ -0,0 +1,65 @@
+let grille=[
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,2,0,0,0,0,0,2,0,2,0,0,2,0],
+    [0,2,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,2,0],
+    [0,0,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,0,0],
+    [0,1,1,0,2,0,2,2,2,2,2,2,2,0,2,0,1,1,0],
+    [0,0,0,0,2,0,2,0,0,1,0,0,2,0,2,0,0,0,0],
+    [2,2,2,2,2,2,2,0,1,1,1,0,2,2,2,2,2,2,2],
+    [0,0,0,0,2,0,2,0,0,1,0,0,2,0,2,0,0,0,0],
+    [0,1,1,0,2,0,2,2,2,2,2,2,2,0,2,0,1,1,0],
+    [0,0,0,0,2,0,2,0,0,0,0,0,2,0,2,0,0,0,0],
+    [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,2,0,2,2,2,2,2,2,2,2,2,2,2,0,2,2,0],
+    [0,0,2,0,2,0,2,0,0,0,0,0,2,0,2,0,2,0,0],
+    [0,2,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,2,0],
+    [0,2,0,0,0,0,0,0,2,0,2,0,0,0,0,0,0,2,0],
+    [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    ];
+    let pacman= {
+        x:5,
+        y:2,
+        direction: 0
+    }
+    pacman.x
+    function affichegrille(){
+        document.getElementById("grille").innerHTML=""
+    for(let i=0;i<22;i++){
+        for(let j=0;j<19;j++){
+            console.log(grille [i][j]);
+            let element= document.createElement("div")
+            if(grille[i][j]==0)
+            {
+                element.className="mur"
+            }
+            else if(grille[i][j]==1) {
+                element.className="sol"
+            }
+            else{
+                element.className="bonbon"
+            }
+            document.getElementById("grille").appendChild(element)
+            element.style.gridRowStart=i+1
+            element.style.gridColumnStart=j+1
+        }
+    }
+    }
+    setInterval(tourdejeu,500)
+    
+function tourdejeu(){
+    affichegrille();
+    affichepacman();
+}
+function affichepacman(){
+    let divpacman= document.createElement("div")
+    divpacman.className= "pacman"
+    document.getElementById("grille").appendChild(divpacman)
+    divpacman.style.gridColumnStart=pacman.x
+    divpacman.style.gridRowStart=pacman.y
+}
